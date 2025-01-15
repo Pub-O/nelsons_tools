82b4dc760fa201af0a6da08a32f8a83ce28c3803
David Samwald <david@samwald.me>
2025-01-15 08:21:56 +0100

    Simplified Script

--- update-repo.sh
+++ update-repo.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
 cd /var/nelsons_tools
-
-behind='git remote update'
-
-if [ ! -n behind ]
-then
-    echo "No Update needed!"
-else
-    echo "Updating Repo!"
-    git pull
-fi
-
-cp update-repo.sh ../update-repo.sh
-
-chmod +x ../update-repo.sh
+echo "Updating Nelsons-Tools!"
+git pull
